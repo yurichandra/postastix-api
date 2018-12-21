cac50b86a57ab9066796b460596291aece1c402b
Adhyaksa Jaya <adhyaksaj@gmail.com>
2018-12-21 15:28:55 +0700

    Added error handler

--- handler/error.go
+++ handler/error.go
@@ -1,7 +1,39 @@
 package handler
 
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
 type errorResponse struct {
-	HTTPStatusCode int `json:"-"`
-	Error          error
-	ErrorText      string `json:"errors"`
+	HTTPStatusCode int    `json:"-"`
+	Error          string `json:"errors"`
+}
+
+func (e *errorResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	render.Status(r, e.HTTPStatusCode)
+	return nil
+}
+
+func createNotFoundResponse(message string) *errorResponse {
+	if message == "" {
+		message = "Resource not found"
+	}
+
+	return &errorResponse{
+		HTTPStatusCode: 404,
+		Error:          message,
+	}
+}
+
+func createBadRequestResponse(message string) *errorResponse {
+	if message == "" {
+		message = "Bad request"
+	}
+
+	return &errorResponse{
+		HTTPStatusCode: 401,
+		Error:          message,
+	}
 }
